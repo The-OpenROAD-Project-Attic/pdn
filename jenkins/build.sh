3a2905d051dc0541bd252312888ea620dc88bbb8
Abdelrahman <abdelrahman@brown.edu>
2019-09-10 18:53:59 -0400

    remove openroad domain from docker image name

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -1,2 +1,2 @@
-docker build -f jenkins/Dockerfile.dev -t openroad/pdn .
-docker run -v $(pwd):/pdn openroad/pdn bash -c "./pdn/jenkins/install.sh"
+docker build -f jenkins/Dockerfile.dev -t pdn .
+docker run -v $(pwd):/pdn pdn bash -c "./pdn/jenkins/install.sh"
