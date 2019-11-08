3b49a019bb596032c956f7ce4986b7d684cf4ac8
Colin Holehouse <colhol01@arm.com>
2019-11-08 11:15:12 +0000

    Need to wait on using helper functions until master branch of OpenDB is updated

--- test/init_db.tcl
+++ test/init_db.tcl
@@ -1,7 +1,13 @@
 set db [dbDatabase_create]
 
-set libs [read_lef $db $env(LEF_FILES)]
-set chip [read_def $libs $env(DEF_FILE)]
+# set libs [read_lef $db $env(LEF_FILES)]
+# set chip [read_def $libs $env(DEF_FILE)]
+
+set lef_parser [new_lefin $db true]
+set def_parser [new_defin $db]
+
+$lef_parser createTechAndLib tech $env(LEF_FILES)
+$def_parser createChip [$db getLibs] $env(DEF_FILE)
 
 export_db $db $env(DESIGN).fp.odb
 
