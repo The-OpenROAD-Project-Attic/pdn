e368fc0a5b7a826e3d194ee91e4c4d71e9786c25
Colin Holehouse <colhol01@arm.com>
2019-11-12 11:15:34 +0000

    Use renamed helper functions

--- test/apply_pdn.tcl
+++ test/apply_pdn.tcl
@@ -4,9 +4,9 @@ source $dir/pkgIndex.tcl
 package require pdn
 
 set db [dbDatabase_create]
-import_db $db $env(DESIGN).fp.odb
+odb_import_db $db $env(DESIGN).fp.odb
 
 set block [[$db getChip] getBlock]
 pdn apply $block PDN.cfg
 
-write_def $block $env(DESIGN)_pdn.def DEF_5_6
+odb_write_def $block $env(DESIGN)_pdn.def DEF_5_6
--- test/init_db.tcl
+++ test/init_db.tcl
@@ -1,13 +1,7 @@
 set db [dbDatabase_create]
 
-# set libs [read_lef $db $env(LEF_FILES)]
-# set chip [read_def $libs $env(DEF_FILE)]
+set libs [odb_read_lef $db $env(LEF_FILES)]
+set chip [odb_read_def $libs $env(DEF_FILE)]
 
-set lef_parser [new_lefin $db true]
-set def_parser [new_defin $db]
-
-$lef_parser createTechAndLib tech $env(LEF_FILES)
-$def_parser createChip [$db getLibs] $env(DEF_FILE)
-
-export_db $db $env(DESIGN).fp.odb
+odb_export_db $db $env(DESIGN).fp.odb
 
